2a9f7f9f756faff9aec127a532598a4897450a42
Pawel Pabich <pawel@pabich.eu>
2016-06-23 10:52:23 +1000

    Removed a bit of code duplication

--- public/tendersmash.js
+++ public/tendersmash.js
@@ -346,12 +346,29 @@ app.controller("mainController", function ($scope, $http, $sce, $q, $timeout, pr
                     return $scope.getComments(baseUrl, currentPage + 1)
                                 .then(function(nextPageComments) {
                                   var comments = currentPageResponse.data.comments.concat(nextPageComments);
-                                  return $q.when(comments);
+                                  return comments;
                                 });
                   } else {
-                    return $q.when([]);
+                    return [];
                   }
                 });
+  }
+
+  $scope.getDiscussions = function(disccussions) {
+    var promises = [];
+    _.each(disccussions, function (disccussion) {
+      promises.push($http.get(disccussion.href)
+          .then(function(response) {
+            var baseUrl = response.data.comments_href.replace("{?page}", "");
+            return $scope.getComments(baseUrl)
+                .then(function(comments) {
+                  response.data.comments = comments;
+                  return response;
+                });
+          }));
+    });
+
+    return promises;
   }
 
   $scope.reload = function() {
@@ -408,36 +425,10 @@ app.controller("mainController", function ($scope, $http, $sce, $q, $timeout, pr
 
             $scope.isListsPopulated = true;
 
-            var unassignedPromises = [];
-            var promises = [];
             var pendingUnassigned = _.filter(pendingDiscussionListing.discussions, function (d) { return d.cached_queue_list.length === 0; });
             var pendingAssigned = _.filter(pendingDiscussionListing.discussions, function (d) { return d.cached_queue_list.length > 0; });
-            
-            _.each(pendingUnassigned, function (d) {
-              unassignedPromises.push($http.get(d.href)
-                                            .then(function(response) {
-                                              var baseUrl = response.data.comments_href.replace("{?page}", "");
-                                              return $scope.getComments(baseUrl)
-                                                            .then(function(comments) {
-                                                              response.data.comments = comments;
-                                                              return $q.when(response);
-                                                            });
-                                            }));
-
-            });
-            
-            _.each(pendingAssigned, function (discus) {
-              promises.push($http.get(discus.href)
-                                  .then(function(response) {
-                                      var baseUrl = response.data.comments_href.replace("{?page}", "");
-                                      return $scope.getComments(baseUrl)
-                                                    .then(function(comments) {
-                                                      response.data.comments = comments;
-                                                      return $q.when(response);
-                                                    });
-                                  }));
-                                  //.success(function (data) { return data; }));
-            });
+            var unassignedPromises = $scope.getDiscussions(pendingUnassigned);
+            var assignedPromises = $scope.getDiscussions(pendingAssigned);
 
             console.debug('[' + (new Date()).toLocaleString() + '] Retrieving unassigned discussions...');
             $q.all(unassignedPromises).then(function (unassignedDiscussions) {
@@ -458,7 +449,7 @@ app.controller("mainController", function ($scope, $http, $sce, $q, $timeout, pr
               $scope.selectList($scope.unassignedList);
               
               console.debug('[' + (new Date()).toLocaleString() + '] Retrieving assigned discussions...');
-              $q.all(promises).then(function (discussions) {
+              $q.all(assignedPromises).then(function (discussions) {
                 console.debug('[' + (new Date()).toLocaleString() + '] Retrieved all assigned discussions...');
                 var firstList = null;
                 _.each(discussions, function (response) {
